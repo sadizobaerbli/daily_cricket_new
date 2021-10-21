d582ba375b79cf0ef025b35b3a03b93484142168
sadizobaerbli <zobaer.batterylowinteractive@gmail.com>
2021-10-21 17:46:45 +0600

    bottom bar added

---
+++ lib/screens/bottom_navigation_bar.dart
@@ -0,0 +1,43 @@
+import 'package:dailycricket_nv/screens/series/series_card.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Container(
+        child: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favourite',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
